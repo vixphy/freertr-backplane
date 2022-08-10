d7851f16e9367c6494a1e071eabe5235961b318e
Edgard da Cunha Pontes <edgardcunha@gmail.com>
2022-08-10 16:13:39 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ RTR=/home/$USER/freeRouter/rtr.jar
 HWSW=$PWD'/configs/'
 
 tmux new-session -d -s freertr-backplane 'java -jar '$RTR' routersc '$HWSW'r1-hw.txt '$HWSW'r1-sw.txt'
-tmux split-window -v -t 0 -p 50
-tmux send 'java -jar '$RTR' routersc '$HWSW'r2-hw.txt '$HWSW'r2-sw.txt' ENTER;
 tmux split-window -h -t 0 -p 50
+tmux send 'java -jar '$RTR' routersc '$HWSW'r2-hw.txt '$HWSW'r2-sw.txt' ENTER;
+tmux split-window -v -t 0 -p 50
 tmux send 'java -jar '$RTR' routersc '$HWSW'r3-hw.txt '$HWSW'r3-sw.txt' ENTER;
 tmux split-window -v -t 2 -p 50
 tmux send 'java -jar '$RTR' routersc '$HWSW'r4-hw.txt '$HWSW'r4-sw.txt' ENTER;
